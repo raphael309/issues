b673562223e76d23bd4251abb34e2d4ca73c048b
raphael309 <raphel.zilberfenig@mail.huji.ac.il>
2026-04-03 11:57:49 +0300

    Added tests

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -8,4 +8,12 @@ defmodule CliTest do
     assert parse_args([ "--help", "anything" ]) == :help
 	assert parse_args([ "-h", "anything" ]) == :help
   end
+  
+  test "Values test" do
+    assert parse_args([ "user", "project", "100" ]) == { "user", "project", 100 }
+  end
+  
+  test "Default value test" do
+    assert parse_args([ "user", "project" ]) == { "user", "project", 4 }
+  end
 end
